fef0484cf7fe2c3f6eff83728e065c0249175182
Maykon Dias <Maykondias2001@gmail.com>
2023-02-06 11:23:31 -0300

    Update

--- math.cpp
+++ math.cpp
@@ -5,27 +5,44 @@
 using namespace std;
 
 class Math{
-	public: void conversor_decimal_para_binario(int decimal){
-		string resultado = "";
-		int binario = 2;
 
-		while(decimal > binario){
-			resultado = to_string(decimal % binario) + resultado;
-            decimal = decimal / binario;
+    public: int base, valor ;
+
+	public: void conversor_decimal_para_binario(){
+		int decimal = valor;
+		int divisor = base;
+		string resultado = "";
+
+		while(decimal > divisor){
+			resultado = to_string(decimal % divisor) + resultado;
+            decimal = decimal / divisor;
 		}
 
 
-        resultado = to_string(decimal % binario) + resultado;
-        resultado = to_string(decimal / binario) + resultado;
+        resultado = to_string(decimal % divisor) + resultado;
+        resultado = to_string(decimal / divisor) + resultado;
 
-		cout << resultado;
+        int limite = resultado.length();
+
+        if(limite < 4){
+            for(int i = 0; i < 4 - limite ; i++){
+                resultado = to_string(0) + resultado;
+            }
+        }
+
+        cout << resultado;
+
 	}
 };
 
 
 int main(){
 	Math *math = new Math();
-    math->conversor_decimal_para_binario(2);
+	cout << "Digite qual sera a base: ";
+	cin >> math->base;
+    cout << "Digite qual sera o numero na base decimal: ";
+    cin >> math->valor;
+    math->conversor_decimal_para_binario();
 	system("PAUSE > nul");
 	return 0;
 }
